d962de7d12ff0fbd405bb5f2edddf5fc91fd8745
lgrevastimi6 <35587986+lgrevastimi6@users.noreply.github.com>
2018-05-18 12:05:04 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Site for Registration</title>
+</head>
+<frameset rows="25%,75%">
+<frame src="topic.html">
+<frameset cols="25%,50%,25%">
+<frame src="images.html">
+<frame src="message.html">
+<frame src="form3.html">
+</frameset>
+</frameset>
+</html>
